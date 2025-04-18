c6b1278a3c9fb09cdbd88b3ed993bd59e42b5f42
Kaitlin Nguyen <cookykn@gmail.com>
2025-04-18 02:05:59 -0700

    added animation to ball and made player able to jump more frequently

--- kaitlin_test/yarn_scene.gd
+++ kaitlin_test/yarn_scene.gd
@@ -5,7 +5,8 @@ extends Node2D
 func _ready() -> void:
 	var cat = get_node('cat') # Replace with function body.
 	cat.init_for_jumping()
-
+	var ball_anim = get_node('yarn_ball/AnimatedSprite2D')
+	ball_anim.play('default')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,5 +15,6 @@ func _process(delta: float) -> void:
 	var animator = get_node("cat/AnimatedSprite2D")
 	if animator.get_frame() == 12:
 		animator.play('default')
-	elif Input.is_action_just_pressed("ui_select") and animator.animation == "default":
+	elif Input.is_action_just_pressed("ui_select") and (animator.animation == "default" or animator.get_frame() >= 7):
+		animator.frame = 0
 		animator.play('jumping')
